6bdf7d3760c5705755a0407826a4e50df979775f
Marek Polak <markalexpolak@gmail.com>
2023-03-23 18:48:22 -0400

    Completed the JavaScript Final Copy

--- Assets/script.js
+++ Assets/script.js
@@ -3,14 +3,20 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var char = "abcdefghijklmnopqrstuvwxyz";
+  var char = "";
+  var charLower = "abcdefghijklmnopqrstuvwxyz";
   var charNum = "1234567890"
   var charSpec = "!@#$%^&*()";
   var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var password = "";    //generatePassword();
+  var password = "";    
   var passwordText = document.querySelector("#password");
   
   // prompts the user for various options about their password
+
+  var l = prompt("Would you like lowercase characters in your password? Please type yes or no.");
+  //forces the user to answer with either yes or no if a random word is given code will not proceed.
+  
+  while (l != "yes" && l!="no") l = prompt("Please type yes or no");
   
   var x = prompt("Would you like special characters in your password? Please type yes or no.");
   
@@ -36,12 +42,12 @@ function writePassword() {
 
   //forces the user to pick a password within the desired character count
 
-  while (passLen < 8 || passLen > 128) {
+  while (!(passLen > 7) || !(passLen < 129)) {
 
     passLen = prompt("Please choose a length between 8 and 128");
 
   }
-
+  if (l == "yes") char += charLower;
   // if user wants special characters special characters are added to the char string
   if (x == "yes") char += charSpec;
   // if user wants uppercase characters uppercase characters are added to the char string
@@ -57,8 +63,16 @@ function writePassword() {
     //takes the character between 2 random consecutive indices within the char string then adds the chosen character to the password.
     password += char.substring(aNum, aNum + 1);
   }
+  //if the user has selected no types of characters tells the user a password cannot be made and resets the write password function.
+  if (char == ""){
+    alert("You need to pick at least 1 character type for your password!");
+    writePassword();
+  }
 
-  passwordText.value = password;
+  else {
+    passwordText.value = password;
+  }
+
 
 }
 
